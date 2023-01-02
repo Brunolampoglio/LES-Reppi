a18e8a7918868af99c8fb210fe7bc75129c440b7
Brunolampoglio <brunovazlampoglio@gmail.com>
2023-01-02 17:26:02 -0300

    Create and show patient data by qr code

--- src/modules/PatientData/controllers/PatientData.controller.ts
+++ src/modules/PatientData/controllers/PatientData.controller.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import { CreatePatientDataService } from '../services/CreatePatientData.service';
+import { CreateQrCodeService } from '../services/CreateQrCode.service';
 import { DeletePatientDataService } from '../services/DeletePatientData.service';
 import { ListPatientDataService } from '../services/ListPatientData.service';
+import { ShowPatientDataService } from '../services/ShowPatientData.service';
 import { UpdatePatientDataService } from '../services/UpdatePatientData.service';
 
 class PatientDataController {
@@ -14,8 +16,6 @@ class PatientDataController {
       const { patientId } = req.params
 
     const createPatientDataService = container.resolve(CreatePatientDataService);
-
-
 
     const patientData = await createPatientDataService.execute({
       colesterol,
@@ -69,6 +69,29 @@ class PatientDataController {
 
     const patientData = await listPatientDataService.execute({
       patientId,
+    });
+
+    return res.status(200).json(patientData);
+  }
+
+  async createQrCode(req: Request, res: Response): Promise<Response> {
+
+    const createQrCodeService = container.resolve(CreateQrCodeService);
+
+    const patientData = await createQrCodeService.execute({
+      patient_id: req.user.id,
+    });
+
+    return res.status(200).json(patientData);
+  }
+
+  async show(req: Request, res: Response): Promise<Response> {
+    const { qr_code } = req.params;
+
+    const showPatientDataService = container.resolve(ShowPatientDataService);
+
+    const patientData = await showPatientDataService.execute({
+      qr_code,
     });
 
     return res.status(200).json(patientData);
---
+++ src/modules/PatientData/services/dto/ShowPatientDataDTO.ts
@@ -0,0 +1,3 @@
+export interface IShowPatientDataDTO {
+  qr_code: string;
+}
