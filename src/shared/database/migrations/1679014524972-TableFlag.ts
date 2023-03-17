c7599a7490e953e94a6d6034c45873782eb6f0d5
Luciano Pongeluppi <lucianopongeluppi@gmail.com>
2023-03-16 21:55:45 -0300

    feat: Migration table flag

---
+++ src/shared/database/migrations/1679014524972-TableFlag.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class TableFlag1679014524972 implements MigrationInterface {
+    name = 'TableFlag1679014524972'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "flags" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_ea7e333c92a55de9e9b8d0b9afd" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "flags"`);
+    }
+
+}
